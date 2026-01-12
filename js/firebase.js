eeeb5412a2ba4d2e8c79109cace81c801efb2851
danielhiga12 <danielhiga121@gmail.com>
2026-01-12 12:09:00 -0300

    Update and rename supabase.js to firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+
+<script>
+firebase.initializeApp({
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO",
+  projectId: "SEU_PROJECT_ID"
+});
+
+const db = firebase.firestore();
+</script>
--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-<script>
-const supabase = supabase.createClient(
-  "https://zfdnobznwzzcpkhbsezi.supabase.co",
-  "zfdnobznwzzcpkhbsezi"
-);
-</script>
-
